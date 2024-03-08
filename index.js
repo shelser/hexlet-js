2475d568bfd1ec191d0a96e17f3b7a7a53130fb1
shelser <shelserg@gmail.com>
2024-03-08 21:02:23 +0300

    add new dir and file in src/index.js; modified index.js and package.json

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import _ from "lodash";
+import last from '.src/index.js';
 
 console.log("Hello, Hexlet!");
 console.log(_.last(["one", "two"]));
+const lastElement = last(array);
+console.log(lastElement);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+export const last = (array) => {
+  return _.last(array);
+};
