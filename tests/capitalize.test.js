f2454fbf47c62523664ff2ed95e86da053c584eb
shelser <shelserg@gmail.com>
2024-03-29 19:37:45 +0300

    change capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,6 @@
+import { strict as assert } from 'node:assert'; 
 import {capitalize} from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('') === '');
+assert(capitalize('hello') === 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-
-}
-
-console.log('Все тесты пройдены!');
